87a98da4538ef8764909da62adfc4ba911127c5c
tasos_theoch <tasos96@gmail.com>
2017-10-29 23:53:09 +0200

    Add files via upload

---
+++ 1stProject/1haskhsh.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8,OUTPUT); //xrhsimopoihsh ths thuras 8 ws thyras eksodou
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(8,HIGH); //Anavei high to led sth thura 8
+  delay(100000); //anamonh 1000ms(1 deuterolepto)
+  digitalWrite(8,LOW); //Anavei low to led sth thura 8
+  delay(500); //anamonh 500ms dhladh miso deuterolepto
+}
---
+++ 2ndProject/2haskhsh.ino
@@ -0,0 +1,41 @@
+//ekxwrhsh stis times blue green kai red tis times twn thurwn pou tha xrhsimopoihsoume.
+int Blue = 11;
+int Green = 10;
+int Red = 9;
+void setup() {
+  // put your setup code here, to run once:
+  //orismos twn output.
+  pinMode(Blue,OUTPUT);
+  pinMode(Green,OUTPUT);
+  pinMode(Red,OUTPUT);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //energopoihsh prasinou xrwmatos
+  analogWrite(Green,255);
+  //paush gia 5 deuterolepta.
+  delay(5000);
+  //apenergopoihsh prasinou xrwmatos.
+  analogWrite(Green,0);
+  //paush gia 10 milliseconds.
+  delay(10);
+  //energopoihsh tou portokali xrwmatos
+  analogWrite(Red,100);
+  analogWrite(Green,20);
+  //paush gia 5 deuterolepta.
+  delay(5000);
+  //apenergopoihsh portokali xrwmatos.
+  analogWrite(Red,0);
+  analogWrite(Green,0);
+  //paush gia 10ms;
+  delay(10);
+  //energopoihsh kokkinou xrwmatos
+  analogWrite(Red,255);
+  //paush gia 5 deuterolepta.
+  delay(5000);
+  //apenergopoihsh tou kokkinou xrwmatos.
+  analogWrite(Red,0);
+  //paush gia 10ms
+  delay(10);
+}
